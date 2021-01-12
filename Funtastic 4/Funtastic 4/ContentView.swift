e3442eda13925e49c060dfac2dd1532854111da2
mohamad7islam <76557504+mohamad7islam@users.noreply.github.com>
2021-01-13 02:52:28 +0300

    a small save

--- Funtastic 4/Funtastic 4/ContentView.swift
+++ Funtastic 4/Funtastic 4/ContentView.swift
@@ -14,44 +14,56 @@
         var body: some View {
             ZStack {
                 NavigationView(){
-                    VStack(){
-                        Spacer()
-                        Image("logo")
-                            .resizable()
-                            .frame(width: 400, height: 400)
-                            .onLongPressGesture {
-                                f1 = true
-                            }
-                        Spacer()
-                        Spacer()
-                        NavigationLink(
-                            destination: playersNameView(),
-                            label: {
-                                Text("play")
-                                    .padding()
-                            }).onLongPressGesture {
-                                f2 = true
-                            }
-                        Spacer()
-                        NavigationLink(
-                            destination: recoredsView(),
-                            label: {
-                                Text("Games Recores")
-                                    .padding()
-                            }).onLongPressGesture {
-                                if f1 && f2 {
-                                    showingPopup = true
+                    ZStack {
+                        Color.black
+                            .ignoresSafeArea()
+                        VStack(){
+                            Spacer()
+                            Image("logo")
+                                .resizable()
+                                .frame(width: 400, height: 400)
+                                .onLongPressGesture {
+                                    f1 = true
                                 }
-                            }
-                        Spacer()
-                        Spacer()
+                            Spacer()
+                            Spacer()
+                            NavigationLink(
+                                destination: playersNameView(),
+                                label: {
+                                    Text("play")
+                                        .font(.custom("rm_playtime", size: 48, relativeTo: .body))
+                                        .foregroundColor(.red)
+                                        .padding()
+                                }).onLongPressGesture {
+                                    f2 = true
+                                }
+                            Spacer()
+                            NavigationLink(
+                                destination: recoredsView(),
+                                label: {
+                                    Text("Games Recores")
+                                        .font(.custom("rm_playtime", size: 48, relativeTo: .body))
+                                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                        .padding()
+                                }).onLongPressGesture {
+                                    if f1 && f2 {
+                                        showingPopup = true
+                                    }
+                                }
+                            Spacer()
+                            Spacer()
+                        }
                     }
                 }
             }.popup(isPresented: $showingPopup, autohideIn: 2) {
                 HStack {
-                    Text("Copied")
+                    Text("Congratulations")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    Text("you found the easter egg , send the sceenshoot to the discored channel to recive your price  ")
+                        .foregroundColor(.white)
                 }
-                .frame(width: 200, height: 60)
+                .frame(width:500, height: 500)
                 .background(Color(red: 1, green: 1, blue: 1))
                 .cornerRadius(30.0)
 }
