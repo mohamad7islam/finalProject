9ae7e1699d026b1a957d8f653eaa5293cf9b7cf6
mohamad7islam <76557504+mohamad7islam@users.noreply.github.com>
2021-01-13 02:24:07 +0300

    added an easter egg :)

--- Funtastic 4/Funtastic 4/ContentView.swift
+++ Funtastic 4/Funtastic 4/ContentView.swift
@@ -8,32 +8,54 @@
     import SwiftUI
     
     struct ContentView: View {
+        @State var showingPopup = false
+        @State var f1 = false
+        @State var f2 = false
         var body: some View {
-            NavigationView(){
-                VStack(){
-                    Spacer()
-                    Image("logo")
-                        .resizable()
-                        .frame(width: 400, height: 400)
-                    Spacer()
-                    Spacer()
-                    NavigationLink(
-                        destination: playersNameView(),
-                        label: {
-                            Text("play")
-                                .padding()
-                        })
-                    Spacer()
-                    NavigationLink(
-                        destination: recoredsView(),
-                        label: {
-                            Text("Games Recores")
-                                .padding()
-                        })
-                    Spacer()
-                    Spacer()
+            ZStack {
+                NavigationView(){
+                    VStack(){
+                        Spacer()
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 400, height: 400)
+                            .onLongPressGesture {
+                                f1 = true
+                            }
+                        Spacer()
+                        Spacer()
+                        NavigationLink(
+                            destination: playersNameView(),
+                            label: {
+                                Text("play")
+                                    .padding()
+                            }).onLongPressGesture {
+                                f2 = true
+                            }
+                        Spacer()
+                        NavigationLink(
+                            destination: recoredsView(),
+                            label: {
+                                Text("Games Recores")
+                                    .padding()
+                            }).onLongPressGesture {
+                                if f1 && f2 {
+                                    showingPopup = true
+                                }
+                            }
+                        Spacer()
+                        Spacer()
+                    }
                 }
-            }
+            }.popup(isPresented: $showingPopup, autohideIn: 2) {
+                HStack {
+                    Text("Copied")
+                }
+                .frame(width: 200, height: 60)
+                .background(Color(red: 1, green: 1, blue: 1))
+                .cornerRadius(30.0)
+}
+
         }
     }
     
